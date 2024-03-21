2c5f0ea60ce56a1c404105eacaf38be40e7da62b
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-21 16:27:59 +0500

    Updated Package Version with @waqas_nazir/first_npm_package@latest

--- Install_Package_In_Project/index.js
+++ Install_Package_In_Project/index.js
@@ -2,6 +2,6 @@ const package = require("@waqas_nazir/first-npm-package");
 
 console.log("My NPM Package Variable Name:", package);
 
-console.log("Name:", package.name);
+console.log("Name:", package.heading);
 
 console.log("Add Numbers Handler:", package.sumValuesHandler(5, 4));
