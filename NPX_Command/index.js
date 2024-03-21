d0dfaf60158a07572fa14e136cc408fb05c9293c
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-21 15:15:13 +0500

    NPX Command

---
+++ NPX_Command/index.js
@@ -0,0 +1,7 @@
+const package = require("@waqas_nazir/first-npm-package");
+
+console.log("My NPM Package Variable Name:", package);
+
+console.log("Name:", package.name);
+
+console.log("Add Numbers Handler:", package.sumValuesHandler(5, 4));
