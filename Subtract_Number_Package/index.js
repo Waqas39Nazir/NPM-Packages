1382da728c0a8dc88ecfb601d4cbb7a5a20a1642
Waqas Nazir <waqasnazir@altaurux.com>
2024-03-21 16:47:25 +0500

    Subtract Numbers Package

---
+++ Subtract_Number_Package/index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.error("Please Enter at least 2 numbers");
+  process.exit(1); //an error occured
+}
+
+const total = args.reduce((a, b) => {
+  return parseFloat(a) - parseFloat(b);
+});
+
+if (isNaN(total)) {
+  console.error("One or more arguments are not numbers");
+  process.exit(1); //an error occured
+}
+
+console.log("Result:", total);
+process.exit(0); // no erros occured
